cbfc410cdd2091edc3e9959aa078e52698569aaa
mengfeifei <happymff@126.com>
2017-03-21 21:40:46 +0800

    just change for test sample

--- src/test/java/DataProvider/DataProvid.java
+++ src/test/java/DataProvider/DataProvid.java
@@ -2,6 +2,7 @@ package DataProvider;
 
 import java.io.IOException;
 import org.testng.annotations.DataProvider;
+import util.ReadExcelJXL;
 import util.ReadExcelPOI;
 
 /**
@@ -9,9 +10,15 @@ import util.ReadExcelPOI;
  */
 public class DataProvid {
 
+
     @DataProvider(name = "DriverInitialize")
     public static Object[][] driverInitialize() throws IOException {
         return ReadExcelPOI.getTestData("/Users/mff/Documents/TestData/TestingForAiXuepaiData", "TestData.numbers", "data");
+    }
+    @DataProvider(name = "SeleniumTest")
+    public static Object[][] getData() throws IOException {
+        ReadExcelJXL readExcelJXL = new ReadExcelJXL();
+        return readExcelJXL.getTestData("/Users/mengfeifei/Desktop/workspace/", "TestData.xls", "data");
     }
     @DataProvider(name = "LoginSucess")
     public static Object[][] loginsucess() {
@@ -34,7 +41,7 @@ public class DataProvid {
     @DataProvider(name = "StudentLogin1")
     public static Object[][] student1loginsucess() {
         return new Object[][] {
-                new Object[] { "s1forff", "a11111" }
+                new Object[] { "s1forff11", "a11111","feifei", "1111110111111111111"}
         };
     }
 }
